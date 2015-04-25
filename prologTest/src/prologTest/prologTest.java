a091746eb016cc7f7f914dbe745ae9185d323fa5
rob0229 <rob0229@gmail.com>
2015-04-25 15:38:33 -0400

    Started adding test JFrames for uer input

--- prologTest/src/prologTest/prologTest.java
+++ prologTest/src/prologTest/prologTest.java
@@ -1,15 +1,48 @@
 package prologTest;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import com.declarativa.interprolog.XSBSubprocessEngine;
 public class prologTest{
+
+public static void main(String args[]) {
+	  JFrame frame = new JFrame("Computer Science Class Scheduler");
+	  JPanel panel = new JPanel();
+	  JLabel label = new JLabel(" Does Rob Like: ");
+	  JTextField answer = new JTextField();
+	  answer.setEditable(true);
+	  answer.setPreferredSize(new Dimension(200, 60));
+	  panel.setBackground(new Color(255,255,255));
+	  panel.add(label);
+	  panel.add(answer);
+	  frame.add(panel);
+		// Add a window listener for close button
+		frame.addWindowListener(new WindowAdapter() {
 	
-  public static void main(String args[]) {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 	  
-	  System.out.println("here");
+	  label.setPreferredSize(new Dimension(600, 600));
+	  //frame.getContentPane().add(label, BorderLayout.CENTER);
+	 // frame.getContentPane().add(answer, BorderLayout.CENTER);
+	  frame.pack();
+	  frame.setVisible(true);
+	  
+	  
 	  
 	  XSBSubprocessEngine engine = new XSBSubprocessEngine(args[0]);
-	  System.out.println("here2");
+
 	  engine.command("import append/3 from basics");
 	  
 	  Object[] bindings = engine.deterministicGoal("name(User,UL),append(\"Hello,\", UL, ML), name(Message,ML)","[string(User)]",new Object[]{System.getProperty("user.name")},"[string(Message)]");
@@ -25,7 +58,7 @@ public class prologTest{
 	  bindings = engine.deterministicGoal("name('"+System.getProperty("user.name")+"',UL),append(\"Hello,\", UL, ML), name(Message,ML)","[string(Message)]");
 	  // (notice the ' surrounding the user name, unnecessary in the first case)
 	  System.out.println("Same:"+bindings[0]);
-	  System.exit(0);
+
   }
   
 }
